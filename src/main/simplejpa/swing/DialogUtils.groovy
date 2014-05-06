a76ba15d8ed09ebfab2ef52c1072b7c62d47d022
Jocki Hendry <jocki.hendry@gmail.com>
2014-05-06 17:19:51 +0700

    Execute onFinish in UI Thread.

--- src/main/simplejpa/swing/DialogUtils.groovy
+++ src/main/simplejpa/swing/DialogUtils.groovy
@@ -21,28 +21,29 @@ class DialogUtils {
         def result = null
         if (args == null) args = [:]
         app.withMVCGroup(mvcGroupName, args) { m, v, c ->
-            Window thisWindow = SwingUtilities.getWindowAncestor(view.mainPanel)
-            JDialog dialog = new JDialog(thisWindow, Dialog.ModalityType.APPLICATION_MODAL)
-            if (contentDecorator) {
-                dialog.contentPane = contentDecorator(v.mainPanel)
-            } else if (DialogUtils.defaultContentDecorator) {
-                dialog.contentPane = DialogUtils.defaultContentDecorator.call(v.mainPanel)
-            } else {
-                dialog.contentPane = v.mainPanel
+            app.execInsideUISync {
+                Window thisWindow = SwingUtilities.getWindowAncestor(view.mainPanel)
+                JDialog dialog = new JDialog(thisWindow, Dialog.ModalityType.APPLICATION_MODAL)
+                if (contentDecorator) {
+                    dialog.contentPane = contentDecorator(v.mainPanel)
+                } else if (DialogUtils.defaultContentDecorator) {
+                    dialog.contentPane = DialogUtils.defaultContentDecorator.call(v.mainPanel)
+                } else {
+                    dialog.contentPane = v.mainPanel
+                }
+
+                // Bind ESC to close dialog action
+                dialog.getRootPane().registerKeyboardAction({ ActionEvent ae ->
+                    dialog.setVisible(false)
+                }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW)
+
+                dialog.pack()
+                dialogProperties?.each { prop, value ->
+                    dialog."$prop" = value
+                }
+                dialog.setLocationRelativeTo(thisWindow)
+                dialog.setVisible(true)
             }
-
-            // Bind ESC to close dialog action
-            dialog.getRootPane().registerKeyboardAction({ ActionEvent ae ->
-                dialog.setVisible(false)
-            }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW)
-
-            dialog.pack()
-            dialogProperties?.each { prop, value ->
-                dialog."$prop" = value
-            }
-            dialog.setLocationRelativeTo(thisWindow)
-            dialog.setVisible(true)
-
             result = onFinish?.call(m, v, c)
         }
         result
