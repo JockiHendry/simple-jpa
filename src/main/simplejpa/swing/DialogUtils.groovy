4a391841a7eb05b8842785ec669e57651e7b29ac
Jocki Hendry <jocki.hendry@gmail.com>
2014-09-10 15:57:55 +0700

    Remember parent JDialog for nested invocation.

--- src/main/simplejpa/swing/DialogUtils.groovy
+++ src/main/simplejpa/swing/DialogUtils.groovy
@@ -8,7 +8,6 @@ import java.awt.Dialog
 import java.awt.Window
 import griffon.core.GriffonView
 import griffon.core.GriffonApplication
-
 import java.awt.event.ActionEvent
 import java.awt.event.KeyEvent
 
@@ -16,36 +15,40 @@ class DialogUtils {
 
     public static Closure defaultContentDecorator = null
 
+    public static JDialog lastDialog = null
+
     static def showMVCGroup(String mvcGroupName, Map args, GriffonApplication app, GriffonView view,
             Map dialogProperties = null, Closure onFinish = null, Closure contentDecorator = null) {
+
         def result = null
         if (args == null) args = [:]
         app.withMVCGroup(mvcGroupName, args) { m, v, c ->
-            app.execInsideUISync {
-                Window thisWindow = SwingUtilities.getWindowAncestor(view.mainPanel)
-                JDialog dialog = new JDialog(thisWindow, Dialog.ModalityType.APPLICATION_MODAL)
-                if (contentDecorator) {
-                    dialog.contentPane = contentDecorator(v.mainPanel)
-                } else if (DialogUtils.defaultContentDecorator) {
-                    dialog.contentPane = DialogUtils.defaultContentDecorator.call(v.mainPanel)
-                } else {
-                    dialog.contentPane = v.mainPanel
-                }
-
-                // Bind ESC to close dialog action
-                dialog.getRootPane().registerKeyboardAction({ ActionEvent ae ->
-                    dialog.setVisible(false)
-                }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW)
-
-                dialogProperties?.each { prop, value ->
-                    dialog."$prop" = value
-                }
-                dialog.pack()
-                dialog.setLocationRelativeTo(thisWindow)
-                dialog.setVisible(true)
+            Window parent = lastDialog ?: SwingUtilities.getWindowAncestor(view.mainPanel)
+            JDialog dialog = new JDialog(parent, Dialog.ModalityType.APPLICATION_MODAL)
+            lastDialog = dialog
+            if (contentDecorator) {
+                dialog.contentPane = contentDecorator(v.mainPanel)
+            } else if (DialogUtils.defaultContentDecorator) {
+                dialog.contentPane = DialogUtils.defaultContentDecorator.call(v.mainPanel)
+            } else {
+                dialog.contentPane = v.mainPanel
             }
+
+            // Bind ESC to close dialog action
+            dialog.getRootPane().registerKeyboardAction({ ActionEvent ae ->
+                dialog.setVisible(false)
+            }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW)
+
+            dialogProperties?.each { prop, value ->
+                dialog."$prop" = value
+            }
+            dialog.pack()
+            dialog.setLocationRelativeTo(parent)
+            dialog.setVisible(true)
             result = onFinish?.call(m, v, c)
         }
+        lastDialog = null
         result
+
     }
 }
