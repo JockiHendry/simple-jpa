d26a50a499805f975f40ad477ad074e9f6942867
Jocki Hendry <jocki.hendry@gmail.com>
2015-01-21 00:53:22 +0700

    Inject `app` to repository if it is defined.

--- src/main/simplejpa/artifact/repository/RepositoryArtifactHandler.java
+++ src/main/simplejpa/artifact/repository/RepositoryArtifactHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Jocki Hendry.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package simplejpa.artifact.repository;
 
 import griffon.core.*;
@@ -67,6 +83,9 @@ public class RepositoryArtifactHandler extends ArtifactHandlerAdapter {
                     throw new NewInstanceCreationException("Could not create a new instance of class " + griffonClass.getClazz().getName(), sanitize(targetException));
                 }
                 instances.put(name, instance);
+                if (griffonClass.hasProperty("app")) {
+                    InvokerHelper.setProperty(instance, "app", getApp());
+                }
                 getApp().addApplicationEventListener(instance);
                 if (triggerEvent) {
                     getApp().event(GriffonApplication.Event.NEW_INSTANCE.getName(), asList(griffonClass.getClazz(), "repository", instance));
