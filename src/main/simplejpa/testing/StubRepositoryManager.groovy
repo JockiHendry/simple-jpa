bac442c35915d49b6acde717102ccff56b5c33f8
Jocki Hendry <jocki.hendry@gmail.com>
2015-02-01 20:53:54 +0700

    Add `StubRepositoryManager`.

---
+++ src/main/simplejpa/testing/StubRepositoryManager.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Jocki Hendry.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+package simplejpa.testing
+
+import simplejpa.artifact.repository.RepositoryManager
+import java.util.concurrent.ConcurrentHashMap
+
+class StubRepositoryManager implements RepositoryManager {
+
+    public final Map<String, Object> instances = new ConcurrentHashMap<>()
+
+    public StubRepositoryManager() {}
+
+    public Object findRepository(String name) {
+        if (!name.endsWith('Repository')) name += 'Repository'
+        instances[name]
+    }
+
+    @Override
+    Object doInstantiate(String name, boolean triggerEvent) {
+        return instances[name]
+    }
+
+    @Override
+    Map<String, Object> getRepositories() {
+        instances
+    }
+
+}
